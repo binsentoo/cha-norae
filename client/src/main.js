9a46786dc0aa361a734c9a6be56751bdf7aae1ee
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2026-09-07 21:13:00 -0700

    live lyrics basic implementation

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,13 @@
 // main.js
 import { startOAuth, handleOAuthCallback, getAccessToken } from './auth.js';
-import { startPolling } from './player.js';
+import { startPolling, getEstimatedProgressMs } from './player.js';
+import { fetchLyrics, getActiveLineIndex } from './lyrics.js';
 
 const params = new URLSearchParams(window.location.search);
 const isCallback = params.has('access_token') || params.has('error');
- 
+
+let currentLyrics = null;
+
 async function init() {
     if (isCallback) {
         const result = await handleOAuthCallback();
@@ -18,13 +21,31 @@ async function init() {
     }
 }
 
-function handleTrackUpdate(track) {
+async function handleTrackUpdate(track) {
     if (track === null) {
-        document.getElementById("current-track").textContent = "no song is playing rn"
+        document.getElementById("current-track").textContent = "no song is playing rn";
+        return;
     }
 
-    document.getElementById("current-track").textContent = track.artist + track.name
+    document.getElementById("current-track").textContent = track.artist + " - " + track.name;
+    currentLyrics = await fetchLyrics(track.name, track.artist, track.album, Math.round(track.durationMs / 1000));
+    document.getElementById("lyrics").textContent = JSON.stringify(currentLyrics);
 }
 
+
+// for lyrics + progress bar
+function renderLoop() {
+    let elapsedSec = getEstimatedProgressMs() / 1000;
+    if (currentLyrics && currentLyrics.synced) {
+        let index = getActiveLineIndex(currentLyrics.lines, elapsedSec);
+        const prevLine = currentLyrics.lines[index - 1]?.text || '';
+        const currLine = currentLyrics.lines[index].text;
+        const nextLine = currentLyrics.lines[index + 1]?.text || '';
+        document.getElementById("lyrics").textContent = prevLine + '\n' + currLine + '\n' + nextLine;
+    }
+    requestAnimationFrame(renderLoop);
+}
+
+requestAnimationFrame(renderLoop);
 
 init()
