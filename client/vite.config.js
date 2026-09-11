69a1cd6a2deb7540b0018beb547fa3cde2c14b18
Vincent Phan <86548242+binsentoo@users.noreply.github.com>
2026-09-10 20:01:52 -0700

    minor changes
    -- removed unneeded settings, fixed small bug with connect-btn

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     server: {
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:3000',
-                changeOrigin: true,
-            }
-        },
         host: '127.0.0.1'
-    },
-    build: {
-        outDir: 'dist'
     }
 })
